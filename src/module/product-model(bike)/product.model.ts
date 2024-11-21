0d45a15e6fff0c0d23a33588177517b5821cba82
gopalbasak1 <gopalbasak1223@gmail.com>
2024-11-21 22:59:11 +0600

    create product model schema

---
+++ src/module/product-model(bike)/product.model.ts
@@ -0,0 +1,36 @@
+import { model, Schema } from 'mongoose';
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+//define and export the productSchema
+const Product = model('Product', productSchema);
+export default Product;
